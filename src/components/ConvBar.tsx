713dd6eab22fe0fedb45d5f2eac013b476d47954
nghialt3670 <nghialt3670@gmail.com>
2024-08-07 10:47:15 +0700

    Fix conversation bar grow overflow

--- src/components/ConvBar.tsx
+++ src/components/ConvBar.tsx
@@ -10,7 +10,7 @@ export default function ConvBar({ children }: { children: ReactNode }) {
 
   return (
     <div
-      className={`h-full ${widthStyle} bg-slate-400 transition-width duration-300 md:relative absolute z-10`}
+      className={`h-[calc(100%-5rem)] ${widthStyle} bg-slate-400 transition-width duration-300 md:relative absolute z-10`}
     >
       {children}
     </div>
